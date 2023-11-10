043f452d1c6fe08ccacc96908c58c67d30969434
samir <luisvqzlab2022@gmail.com>
2023-11-10 11:27:55 -0500

    vd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <h1>honorio delgado espinoza</h1>
+      <h1>TURNO dia</h1>
+      <h1>10/11/2023</h1>
+      <h1>Luis Samuel Vasquez Labra</h1>
+    </div>
+  )
+}
+
+export default App
